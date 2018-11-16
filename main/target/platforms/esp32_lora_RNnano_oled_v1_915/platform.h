9714804c4682b0fb0ba42c6be0017c9895e31ea2
Tanjun <32965926@qq.com>
2018-11-16 12:02:07 +0800

    Modify  nano 915 HAL_GPIO_USER_MASK

---
+++ main/target/platforms/esp32_lora_RNnano_oled_v1_915/platform.h
@@ -0,0 +1,6 @@
+#include "target/bands/868_915.h"
+
+#include "target/platforms/esp32_lora_RNnano_v1/common.h"
+#include "target/platforms/esp32_lora_RNnano_v1/screen.h"
+
+#define BOARD_NAME "RNRC nano V1 915MHz"
